412d772e5d691bd8534b5d93da784d601e8d43f7
zcarter <zachariahcarter07@gmail.com>
2024-12-10 22:11:51 -0500

    1st

---
+++ src/main/java/net/shattered/rinth/Netherinth.java
@@ -0,0 +1,15 @@
+package net.shattered.rinth;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Netherinth implements ModInitializer {
+	public static final String MOD_ID = "rinth";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/shattered/rinth/NetherinthClient.java
@@ -0,0 +1,10 @@
+package net.shattered.rinth;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NetherinthClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
