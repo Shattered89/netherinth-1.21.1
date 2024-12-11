46203709647b5dc9ea4c529b4afefef835ae622c
zcarter <zachariahcarter07@gmail.com>
2024-12-11 00:51:55 -0500

    Added Custom Items!

--- src/main/java/net/shattered/rinth/Netherinth.java
+++ src/main/java/net/shattered/rinth/Netherinth.java
@@ -2,6 +2,7 @@ package net.shattered.rinth;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.shattered.rinth.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,6 @@ public class Netherinth implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
